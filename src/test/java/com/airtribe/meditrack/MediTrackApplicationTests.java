ab273268b866e78283c04216e4f0a62f18a49868
rvk16 <rahul.karma121@gmail.com>
2026-02-12 23:49:27 +0530

    Initial commit

---
+++ src/test/java/com/airtribe/meditrack/MediTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airtribe.meditrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediTrackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
